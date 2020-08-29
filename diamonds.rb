d20ac00e968386121d38003ebf7db4297ab09bc2
devin <devinbremmeyr@gmail.com>
2020-08-29 11:44:30 -0400

    started diamonds problem

---
+++ diamonds.rb
@@ -0,0 +1,13 @@
+# Write a method that displays a 4-pointed diamond in an n x n gird, where n is
+# an odd integer that is supplied as an argument to the method. You may assume
+# that the argument will always be an odd integer.
+
+
+
+def display_diamond(grid_size)
+  
+end
+
+# display_diamond(1)
+# display_diamond(3)
+display_diamond(9)
